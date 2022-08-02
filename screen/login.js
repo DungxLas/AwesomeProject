bdfb99c6073cd20b8511b104e8f6312fe66db599
DungxLas <phamjhungfdungx@gmail.com>
2022-08-02 16:08:55 +0700

    update

--- screen/login.js
+++ screen/login.js
@@ -19,17 +19,15 @@ const WelcomeScreen = () => {
     >
         <View
             style={{
-                flex: 0.3,
+                flex: 0.2,
                 flexDirection: "column",
-                backgroundColor: 'rgba(225,225,225,255)',
+                backgroundColor: 'rgba(228,233,240,255)',
             }}
         >
-            <View style={{
-                flex: 0.15,
-            }} ></View>
+            <View style={{ flex: 0.35 }} />
             <View
                 style={{
-                    flex: 0.45,
+                    flex: 0.5,
                     flexDirection: 'row',
                 }}
             >
@@ -37,15 +35,20 @@ const WelcomeScreen = () => {
                     source={require('../assets/totem.png')}
                     style={{
                         flex: 0.37,
-                        resizeMode: 'contain',
-                        alignSelf: 'flex-end',
+                        resizeMode: 'center',
                         width: Dimensions.get('screen').width * 0.25,
                         height: Dimensions.get('screen').width * 0.25,
                     }}
                 ></Image>
-                <View style={{ flex: 0.63 }} >
+                <View style={{
+                    flex: 0.63,
+                }} >
                     <Text
-                        style={styles.text1}
+                        style={{
+                            color: "black",
+                            fontSize: 55,
+                            fontWeight: "bold",
+                        }}
                     >
                         amor pet
                     </Text>
@@ -53,7 +56,7 @@ const WelcomeScreen = () => {
             </View>
             <View
                 style={{
-                    flex: 0.4,
+                    flex: 0.15,
                 }}
             >
                 <Text
@@ -66,20 +69,23 @@ const WelcomeScreen = () => {
         <ImageBackground
             source={require('../assets/welcome.png')}
             style={{
-                flex: 0.7,
-                justifyContent: "center",
-                resizeMode: 'stretch',
+                flex: 0.8,
+                resizeMode: 'contain',
                 flexDirection: 'column'
             }}
         >
-            <View style={{ flex: 0.15 }} >
+            <View style={{ flex: 0.15 }} ></View>
+            <View style={{
+                flex: 0.15,
+                justifyContent: 'flex-end',
+            }} >
                 <TouchableOpacity
                     style={{
                         alignItems: "center",
                         backgroundColor: "rgba(41,40,59,255)",
                         margin: 16,
                         padding: 17,
-                        borderRadius: 20,
+                        borderRadius: 30,
                     }}
                     onPress={null}
                 >
@@ -90,15 +96,43 @@ const WelcomeScreen = () => {
                     </Text>
                 </TouchableOpacity>
             </View>
-            <View style={{ backgroundColor: 'red', flex: 0.1 }} ></View>
-            <View style={{ flex: 0.15 }} >
+            <View style={{ flex: 0.1, flexDirection: 'row' }} >
+                <View style={{
+                    flex: 0.45,
+                    height: 1,
+                    width: 1,
+                    margin: 15,
+                    backgroundColor: 'black',
+                    alignSelf: 'center',
+                }} />
+                <View style={{ flex: 0.1, justifyContent: 'center' }} >
+                    <Text
+                        style={{
+                            fontSize: 20,
+                            textAlign: 'center',
+                        }}
+                    > or </Text>
+                </View>
+                <View style={{
+                    flex: 0.45,
+                    height: 1,
+                    width: 1,
+                    margin: 15,
+                    backgroundColor: 'black',
+                    alignSelf: 'center',
+                }} />
+            </View>
+            <View style={{
+                flex: 0.15,
+                justifyContent: 'flex-start',
+            }} >
                 <TouchableOpacity
                     style={{
                         alignItems: "center",
                         backgroundColor: "rgba(236,57,57,255)",
                         margin: 16,
                         padding: 17,
-                        borderRadius: 20,
+                        borderRadius: 30,
                     }}
                     onPress={null}
                 >
@@ -109,16 +143,32 @@ const WelcomeScreen = () => {
                     </Text>
                 </TouchableOpacity>
             </View>
-            <View style={{ backgroundColor: 'red', flex: 0.15 }} >
-                {/* <Text
+            <View
+                style={{
+                    flex: 0.1,
+                    justifyContent: 'flex-start',
+                }} >
+                <Text
                     style={styles.text2}
                 >
-                    アカウントを作成すると, 利用規約, に同意することになります
-                </Text> */}
+                    アカウントを作成すると,
+                </Text>
+                <Text
+                    style={styles.text2}
+                >
+                    利用規約, に同意することになります
+                </Text>
             </View>
-            <View style={{ backgroundColor: 'blue', flex: 0.15 }} >
-                {/* <TouchableOpacity
-                    style={styles.button1}
+            <View style={{ flex: 0.15 }} >
+                <TouchableOpacity
+                    style={{
+                        alignItems: "center",
+                        margin: 16,
+                        padding: 17,
+                        borderRadius: 30,
+                        borderColor: 'white',
+                        borderWidth: 1,
+                    }}
                     onPress={null}
                 >
                     <Text
@@ -126,9 +176,9 @@ const WelcomeScreen = () => {
                     >
                         アカウント引き継ぎの方はこちら
                     </Text>
-                </TouchableOpacity> */}
+                </TouchableOpacity>
             </View>
-            <View style={{ backgroundColor: 'red', flex: 0.3 }} ></View>
+            <View style={{ flex: 0.2 }} ></View>
         </ImageBackground>
     </View>
 }
@@ -142,14 +192,6 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         resizeMode: 'contain',
     },
-    text1: {
-        color: "black",
-        fontSize: 55,
-        lineHeight: 100,
-        fontWeight: "bold",
-        textAlign: "left",
-        alignItems: 'flex-end'
-    },
     text2: {
         color: "white",
         fontSize: 16,
@@ -161,12 +203,6 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: "bold",
         textAlign: "center",
-    },
-    button1: {
-        alignItems: "center",
-        backgroundColor: "rgba(41,40,59,255)",
-        padding: 10,
-
     },
 })
 
